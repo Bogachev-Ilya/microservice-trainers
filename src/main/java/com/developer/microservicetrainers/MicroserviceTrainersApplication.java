8b9b33b60121931b549a9d45d8ed2c1acb8fb292
Ilya Bogachev <bogachev_i_i@mail.ru>
2020-07-06 22:22:13 +0300

    initial commit

---
+++ src/main/java/com/developer/microservicetrainers/MicroserviceTrainersApplication.java
@@ -0,0 +1,13 @@
+package com.developer.microservicetrainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTrainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTrainersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developer/microservicetrainers/MicroserviceTrainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.microservicetrainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTrainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
